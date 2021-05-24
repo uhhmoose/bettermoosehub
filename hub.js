e9e7c0be084cbe0e40b3fb19397b480bdf30b6b9
uhhmoose <73522635+uhhmoose@users.noreply.github.com>
2021-05-24 14:53:02 +0500

    Update hub.js

--- hub.js
+++ hub.js
@@ -96,6 +96,36 @@ footer: {
     if (command === `${prefix}direct`) {
         client.users.cache.get(`691875333112070144`).send('hello <a:rindhoo:792421810142445618>')
     }
+    if (command === `${prefix}confess`) {
+        const cfs = args.join(' ');
+        if (message.channel.type != 'dm' && !cfs ) {
+            message.channel.send({embed: {
+                color: '#000000',
+                author: {
+                  name: "",
+                  icon_url: ""
+                },
+                title: "Command \: +confess",
+                description: "Sends an anonymous confession to the confessions channel.",
+                fields: [{
+                    name: "How to use",
+                    value: "Format\: \`+confess \(what you want to confess)\`\n This sends what ever you write after the command into the confessions channel\n ***\(This command only works in the dms of the bot\)***"
+                  }
+                ],
+                timestamp: new Date(),
+                footer: {
+                  icon_url: "",
+                  text: "if you\'re reading this you\'re gay"
+                }
+                }})}
+                else if (message.channel.type != 'dm') {
+                    message.reply('***this command only works in my dms smh***')
+                } else if (!cfs) {
+                    message.reply('i can\'t send an empty confession smh');
+                } else {
+                    client.channels.cache.get('771689620118437908').send(cfs);
+                }
+    }
     
     //if (message.content) {
     //    const profane = !!badwords.find((word) => {
